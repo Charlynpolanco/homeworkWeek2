f8d5ead41d226638d1e03dc20b51bbe19a7d6f8e
Charlynpolanco <110350236+Charlynpolanco@users.noreply.github.com>
2022-08-19 09:32:57 -0400

    Add files via upload

---
+++ hw_Thurs18.js
@@ -0,0 +1,64 @@
+// class Cat {
+//     constructor(color, fluffiness, eyes, weight) {
+//         this.color = color;
+//         this.fluffiness = fluffiness;
+//         this.eyes = eyes;
+//         this.weight = weight;
+//     }
+// size () {
+//     console.log("I'm chonk")
+// }
+// food () {
+//     this.weight += "+"
+// }
+// home () {
+//     console.log("I belong to the box")
+// }
+// }
+// //------------------------Method--------------------->
+// const Mikey = new Cat("Grey", "long", "yellow", "chonk")
+// Mikey.food()
+// console.log(Mikey)
+// Mikey.size()
+// Mikey.home()
+// //-----------------------new cat--------------------->
+// const Kuro = new Cat("Orange", "long", "green", "chonk")
+// Kuro.food()
+// console.log(Kuro)
+// Kuro.size()
+// Kuro.home()
+
+//-----------------------Pirate class--------------------->
+// create 3 properties that are set by the constructor
+// create 3 methods
+// instantiate 2 arrays of 3 pirates, name the arrays after your favorite pirate ships, jollyRoger and blackPearl
+// loop over each array and print 3 properties of each pirate
+class Pirate {
+
+    constructor(crewSize, Captain, treassure){
+      this.crewSize;
+      this.Captain;
+      this.treassure;
+      this.boat;
+    }
+    crackenAttack() {
+          this.boat="The boat was destroyed"
+    }
+    let JollyRoger = [
+      
+    ]
+  
+
+// const White_Beard = new Pirate(5,"White Beard", "The power of Friendship")
+//   pirateList=["White beard", "The black pearl"]
+//   White_Beard.boat= "The black pearl"
+//   White_Beard.crackenAttack()
+//   console.log(pirateList)
+//   console.log(White_Beard)
+  
+   
+    
+  
+  
+  
+  
